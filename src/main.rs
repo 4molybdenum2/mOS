a08f716a7f18ac60690007855fcdf91fa4b5b83e
Tathagata Paul <tathagatapaul7@gmail.com>
2024-02-19 12:52:50 -0500

    separate test module
    Signed-off-by: Tathagata Paul <tathagatapaul7@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,11 @@
 #![no_std]
 #![no_main]
-#![reexport_test_harness_main = "test_main"]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
-
-mod vga_buffer;
-mod serial;
+#![test_runner(mOS::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-// we have to define our own panic handler, PanicInfo parameter has the file and the line number where panic has occured
-// the function should never return, so it is marked as a diverging function return never "!" type
-#[cfg(not(test))] 
-#[panic_handler]
-fn panic(_info : &PanicInfo) -> ! {
-    println!("{}", _info);
-
-    loop {}
-}
-
-
-// our panic handler in test mode
-#[cfg(test)]
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", _info);
-    exit_qemu(QemuExitCode::Failure);
-    loop {}
-}
+use mOS::println;
 
 // this function doesn't return because it is the entry point and is called by the os or the bootloader
 // , and thus should invoke the exit() system call of the os.
@@ -44,6 +22,23 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+
+// we have to define our own panic handler, PanicInfo parameter has the file and the line number where panic has occured
+// the function should never return, so it is marked as a diverging function return never "!" type
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    mOS::test_panic_handler(info)
+}
+
 /*
 linker is a program that combines the generated code into an executable
 the linker thinks that our program depends on the C runtime which is doesnt
@@ -57,53 +52,3 @@ it also needs to switch the CPU from 16 bit real mode into 32 bit protected mode
 it then queries some info such as memory map from BIOS and passes it to OS kernel 
 */
 
-
-
-
-// Test runner
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-
-
-
-    exit_qemu(QemuExitCode::Success);
-
-}
-
-
-// Trivial test case
-#[test_case]
-fn trivial_assertion() {
-    serial_println!("trivial assertion ....");
-    assert_eq!(1, 1);
-    serial_println!("[ok]");
-}
-
-
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-
-pub  enum QemuExitCode {
-    Success = 0x10,
-    Failure = 0x11,
-}
-
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-/* 
-The functionality of the isa-debug-exit device is very simple. When a value is written to the I/O port specified by iobase, 
-it causes QEMU to exit with exit status (value << 1) | 1. So when we write 0 to the port,QEMU will exit with exit status 
-(0 << 1) | 1 = 1, and when we write 1 to the port, it will exit with exit status (1 << 1) | 1 = 3.
-*/
